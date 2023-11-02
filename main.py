c92adc4ae1b26cd566bfba66f7c4c976eac9186a
adityasirji <146421388+adityasirji@users.noreply.github.com>
2023-11-03 04:29:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    APP_ID = "25549663"
-    API_HASH = "b6afbacfe352f8e1888b39bf9884edd9"
-    BOT_TOKEN = "6262813557:AAHE9V3c7JyERs1iCIl8R3XzybJnu4Xynqc"
+    APP_ID = "29257953"
+    API_HASH = "31dae8eac39b38583898e3bc78b96406"
+    BOT_TOKEN = "6923322271:AAEWIo-UnLsZ8SKc0SxANr9M8dfFeo6VOfo"
